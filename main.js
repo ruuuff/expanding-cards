8fbcf6fea51cad32291ed7b5247d21bb9f149712
RuuuFF <ruuufffaria@gmail.com>
2020-12-03 15:25:10 -0300

    Expanding Cards

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// Pega todos os elementos com a classe "panel"
+// Pega todos os elementos com a classe "panel" e armazena em "panels"
 const panels = document.querySelectorAll(".panel");
 
 // Para cada elemento dentro de panels ele executa uma função
 panels.forEach(panel => {
-  // Adiciona o ouvidor de eventos ('click') em cada elemento dentro de panels
+  // Adiciona um ouvidor de eventos ('click') em cada elemento dentro de panels
   panel.addEventListener('click', () => {
     // Chama a função removeActiveClasses
     removeActiveClasses();
